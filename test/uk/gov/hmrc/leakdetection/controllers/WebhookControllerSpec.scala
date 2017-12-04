95f81e6891506ee2576c9da4eb25af0836a33ae0
Konrad <wudkowski@gmail.com>
2017-12-04 20:45:20 +0000

    PLATOPS-1141: Add fake github server for testing

--- test/uk/gov/hmrc/leakdetection/controllers/WebhookControllerSpec.scala
+++ test/uk/gov/hmrc/leakdetection/controllers/WebhookControllerSpec.scala
@@ -19,23 +19,30 @@ package uk.gov.hmrc.leakdetection.controllers
 import ModelFactory._
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.StreamConverters
 import akka.util.Timeout
 import com.typesafe.config.ConfigFactory
 import concurrent.duration._
+import java.io.{BufferedOutputStream, ByteArrayInputStream, ByteArrayOutputStream}
+import java.util.UUID
+import java.util.zip.{ZipEntry, ZipOutputStream}
 import org.apache.commons.codec.digest.HmacUtils
 import org.scalatest.{Matchers, WordSpec}
 import org.scalatestplus.play.OneAppPerSuite
+import play.api._
 import play.api.inject.guice.GuiceApplicationBuilder
-import play.api.test.Helpers.CONTENT_TYPE
+import play.api.mvc.{Action, Results}
+import play.api.test.Helpers.{CONTENT_DISPOSITION, CONTENT_TYPE}
 import play.api.test.{FakeRequest, Helpers}
-import play.api.{Application, Configuration}
+import play.core.server.{NettyServer, ServerConfig}
+import play.api.routing.sird._
 
 class WebhookControllerSpec extends WordSpec with Matchers with OneAppPerSuite with Fixtures {
 
   "Github Webhook" should {
-    "work e2e" in {
-
-      val githubRequest: String = asJson(aPayloadDetails)
+    "work e2e" in withFakeGithub { archiveUrl =>
+      val githubRequest: String =
+        asJson(aPayloadDetails.copy(archiveUrl = archiveUrl, isPrivate = true))
 
       val req =
         FakeRequest("POST", "/leak-detection/validate")
@@ -96,4 +103,68 @@ trait Fixtures { self: OneAppPerSuite =>
       )
       .build
 
+  val server = NettyServer.fromRouter(
+    new ServerConfig(
+      rootDir    = new java.io.File("."),
+      port       = Some(0),
+      sslPort    = None,
+      address    = "0.0.0.0",
+      mode       = play.api.Mode.Test,
+      properties = System.getProperties,
+      configuration = play.api.Configuration(
+        "play.server.netty" -> Map(
+          "maxInitialLineLength" -> 4096,
+          "maxHeaderSize"        -> 8192,
+          "maxChunkSize"         -> 8192,
+          "log.wire"             -> false,
+          "eventLoopThreads"     -> 0,
+          "transport"            -> "jdk",
+          "option.child"         -> Map()
+        )
+      )
+    )) {
+    case GET =>
+      Action {
+        Results.Ok
+          .chunked(StreamConverters.fromInputStream(createZip))
+          .withHeaders(
+            CONTENT_TYPE        -> "application/zip",
+            CONTENT_DISPOSITION -> s"attachment; filename = test.zip"
+          )
+      }
+  }
+
+  type ArchiveUrl = String
+
+  def withFakeGithub(f: ArchiveUrl => Any): Any =
+    try {
+      f(s"http://localhost:${server.httpPort.get}")
+    } finally {
+      server.stop
+    }
+
+  var zippedFiles = List.empty[TestZippedFile]
+
+  def createZip(): ByteArrayInputStream = {
+    val baos = new ByteArrayOutputStream()
+    val zos  = new ZipOutputStream(new BufferedOutputStream(baos))
+
+    try {
+      zippedFiles.foreach { file =>
+        zos.putNextEntry(new ZipEntry(file.path))
+        zos.write(file.content.getBytes("UTF-8"))
+        zos.closeEntry()
+      }
+    } finally {
+      zos.close()
+    }
+
+    new ByteArrayInputStream(baos.toByteArray)
+  }
+
 }
+
+case class TestZippedFile(
+  content: String,
+  path: String = s"repo/${UUID.randomUUID().toString}"
+)
