537fa8dc80c28db4d177da838be78fb18825dd2c
Jakob Grunig <jakob.grunig@digital.hmrc.gov.uk>
2018-01-22 12:27:56 +0000

    PLATOPS-1173 - improved scanning script

--- .scripts/scan-private.sh
+++ .scripts/scan-private.sh
@@ -4,17 +4,20 @@ if [ -z "$GITHUB_TOKEN" ]; then
     echo "Error: set GITHUB_TOKEN environment variable with a github personal access token"
 else
 
-    curl -H "Authorization: token $GITHUB_TOKEN" -s 'https://api.github.com/orgs/hmrc/repos?type=private&per_page=100' | jq .[] | jq -r '.name' > /tmp/private-repositories
-    curl -H "Authorization: token $GITHUB_TOKEN" -s 'https://api.github.com/orgs/hmrc/repos?type=private&per_page=100&page=2' | jq .[] | jq -r '.name' >> /tmp/private-repositories
-    curl -H "Authorization: token $GITHUB_TOKEN" -s 'https://api.github.com/orgs/hmrc/repos?type=private&per_page=100&page=3' | jq .[] | jq -r '.name' >> /tmp/private-repositories
-    curl -H "Authorization: token $GITHUB_TOKEN" -s 'https://api.github.com/orgs/hmrc/repos?type=private&per_page=100&page=4' | jq .[] | jq -r '.name' >> /tmp/private-repositories
+    PAGES_COUNT=$(curl -I -H "Authorization: token $GITHUB_TOKEN" -s 'https://api.github.com/orgs/hmrc/repos?type=private&per_page=100&page=1' | grep Link | perl -nle 'print $1 if /&page=(\d+)>; rel="last/')
+
+    echo "Getting the list of private repositories in $PAGES_COUNT batches"
+    rm /tmp/private-repositories
+
+    for ((i=1; i<=$PAGES_COUNT; i++)); do
+        curl -H "Authorization: token $GITHUB_TOKEN" -s "https://api.github.com/orgs/hmrc/repos?type=private&per_page=100&page=$i" | jq .[] | jq -r '.name' >> /tmp/private-repositories
+    done
 
     while read repository; do
-      echo ""
-      echo ""
       echo "Scanning $repository"
-      curl -s -o /dev/null -w "%{http_code}" "http://localhost:8855/admin/validate/private/$repository/master"
-
+      LEAK_COUNT=$(curl -s "http://localhost:8855/admin/validate/private/$repository/master"  | jq '.inspectionResults | length')
+      echo "$LEAK_COUNT potential problems found on $repository"
+      echo ""
     done </tmp/private-repositories
 
 fi
