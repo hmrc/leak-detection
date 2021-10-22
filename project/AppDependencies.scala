5452fa4e26440211923270a7523f5ac1fd96c575
Richard Sherman <23365258+RikSherman@users.noreply.github.com>
2021-10-22 11:19:35 +0100

    Update resolvers

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,13 +3,14 @@ import sbt._
 
 object AppDependencies {
 
-  val bootstrapVersion = "4.0.0"
-  val hmrcMongoVersion = "0.40.0"
+  val bootstrapVersion = "5.16.0"
+  val hmrcMongoVersion = "0.55.0"
 
   val compile = Seq(
-    "uk.gov.hmrc"           %% "bootstrap-backend-play-27"         % bootstrapVersion,
-    "uk.gov.hmrc"           %% "play-ui"                           % "8.21.0-play-27",
-    "uk.gov.hmrc.mongo"     %% "hmrc-mongo-work-item-repo-play-27" % hmrcMongoVersion,
+    "uk.gov.hmrc"           %% "auth-client"                       % "5.7.0-play-28",
+    "uk.gov.hmrc"           %% "bootstrap-backend-play-28"         % bootstrapVersion,
+    "uk.gov.hmrc"           %% "play-ui"                           % "9.7.0-play-28",
+    "uk.gov.hmrc.mongo"     %% "hmrc-mongo-work-item-repo-play-28" % hmrcMongoVersion,
     "com.github.pureconfig" %% "pureconfig"                        % "0.8.0",
     "org.zeroturnaround"    %  "zt-zip"                            % "1.14",
     "commons-lang"          %  "commons-lang"                      % "2.6",
@@ -22,10 +23,10 @@ object AppDependencies {
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-27"   % bootstrapVersion    % Test,
+    "uk.gov.hmrc"            %% "bootstrap-test-play-28"   % bootstrapVersion    % Test,
     "com.typesafe.play"      %% "play-test"                % PlayVersion.current % Test,
     "org.mockito"            %% "mockito-scala"            % "1.10.2"            % Test,
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-27"  % hmrcMongoVersion    % Test,
+    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28"  % hmrcMongoVersion    % Test,
     "org.scalatestplus.play" %% "scalatestplus-play"       % "4.0.3"             % Test,
     "org.scalatestplus"      %% "scalatestplus-scalacheck" % "3.1.0.0-RC2"       % Test,
     "org.scalacheck"         %% "scalacheck"               % "1.13.4"            % Test,
