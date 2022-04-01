3d4e4cc1a198ba94c89030570d51ac4f94d041ee
Phil Evans <65957825+hmrc-phil@users.noreply.github.com>
2022-04-01 13:46:08 +0100

    BDOG-1785 - Repos with leaks bug (#87)
    * BDOG-1785 only include repos with unresolved leaks in repository endpoint
    
    * BDOG-1785 remove unused parameter values

--- test/uk/gov/hmrc/leakdetection/persistence/LeakRepositorySpec.scala
+++ test/uk/gov/hmrc/leakdetection/persistence/LeakRepositorySpec.scala
@@ -21,12 +21,14 @@ import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import uk.gov.hmrc.leakdetection.IncreasingTimestamps
+import uk.gov.hmrc.leakdetection.ModelFactory.aLeak
 import uk.gov.hmrc.leakdetection.model.{Leak, LeakUpdateResult}
 import uk.gov.hmrc.mongo.test.{CleanMongoCollectionSupport, PlayMongoRepositorySupport}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
-class LeakRepositorySpec extends AnyWordSpec
+class LeakRepositorySpec
+    extends AnyWordSpec
     with Matchers
     with PlayMongoRepositorySupport[Leak]
     with CleanMongoCollectionSupport
@@ -40,7 +42,25 @@ class LeakRepositorySpec extends AnyWordSpec
   "Leak repository" should {
 
     "return zero inserts and not fail when updating with no leaks" in {
-      repository.update("test", "main", Seq.empty).futureValue shouldBe LeakUpdateResult(0,0)
+      repository.update("test", "main", Seq.empty).futureValue shouldBe LeakUpdateResult(0, 0)
+    }
+
+    "return repositories with unresolved leaks" when {
+      "leaks isExcluded is false" in {
+        repository.collection
+          .insertMany(
+            Seq(
+              aLeak(repoName = "repo1").copy(isExcluded = false),
+              aLeak(repoName = "repo2").copy(isExcluded = true)
+            )
+          )
+          .toFuture
+          .futureValue
+
+        val result = repository.findDistinctRepoNamesWithUnresolvedLeaks().futureValue
+
+        result shouldBe Seq("repo1")
+      }
     }
   }
 }
