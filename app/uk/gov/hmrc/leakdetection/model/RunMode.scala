800e630ce5d2d8e9ec273180f0f3bddaba99cd94
Phil Evans <65957825+hmrc-phil@users.noreply.github.com>
2022-04-12 09:49:10 +0100

    BDOG-1809 pass a RunMode as a query param

--- app/uk/gov/hmrc/leakdetection/model/RunMode.scala
+++ app/uk/gov/hmrc/leakdetection/model/RunMode.scala
@@ -17,27 +17,47 @@
 package uk.gov.hmrc.leakdetection.model
 
 import play.api.libs.json._
+import play.api.mvc.QueryStringBindable
 
-sealed trait RunMode {
+sealed trait RunMode extends Product with Serializable {
   def asString: String
 }
 
 object RunMode {
-  case object Normal extends RunMode { override def asString = "normal" }
-  case object Draft extends RunMode { override def asString = "draft" }
+  final case object Normal extends RunMode { override def asString = "normal" }
+  final case object Draft extends RunMode { override def asString  = "draft" }
+
+  def parse(s: String): Either[String, RunMode] =
+    s match {
+      case "normal" => Right(Normal)
+      case "draft"  => Right(Draft)
+      case rm       => Left(s"Invalid run mode: $rm - should be one of: normal, draft")
+    }
 
   val format: Format[RunMode] =
     new Format[RunMode] {
       override def reads(json: JsValue): JsResult[RunMode] =
         json match {
-          case JsString("normal") => JsSuccess(Normal)
-          case JsString("draft") => JsSuccess(Draft)
-          case JsString(rm) => JsError(s"Invalid run mode: $rm - should be one of: normal, draft")
-          case _ => JsError("String value expected")
+          case JsString(s) => parse(s).fold(msg => JsError(msg), rm => JsSuccess(rm))
+          case _           => JsError("String value expected")
         }
 
       override def writes(rm: RunMode): JsValue =
         JsString(rm.asString.toLowerCase())
     }
 
+  implicit def queryStringBindable(implicit stringBinder: QueryStringBindable[String]): QueryStringBindable[RunMode] =
+    new QueryStringBindable[RunMode] {
+      override def bind(key: String, params: Map[String, Seq[String]]): Option[Either[String, RunMode]] =
+        for {
+          value <- stringBinder.bind(key, params)
+        } yield {
+          value match {
+            case Right(rm) => parse(rm)
+            case _         => Left(s"Unable to bind runMode")
+          }
+        }
+
+      override def unbind(key: String, value: RunMode): String = value.asString
+    }
 }
